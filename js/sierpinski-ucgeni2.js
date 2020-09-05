dc013a6ab501d045c48e21327a9586aa0de93988
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-05 18:54:43 +0300

    duzenlendi2

--- js/sierpinski-ucgeni2.js
+++ js/sierpinski-ucgeni2.js
@@ -1,7 +1,8 @@
 
 function loop(value,result,doo,test,update,body){
-    if(test(value) == false){
-              return(body(result));}
+        if(test(value) == false){
+              return(body(result));
+        }
         else {    
           value=update(value);
           result=doo(result);
@@ -9,9 +10,9 @@ function loop(value,result,doo,test,update,body){
           doo(result);
         };
           
-          loop(value,result,result=>(result*3)+1,n => n >=0, n => n - 1, console.log);
-      }
-      loop(4,0,result=>(result*3)+1,n => n>=0, n => n - 1, console.log)
+loop(value,result,result=>(result*3)+1,n => n >=0, n => n - 1, console.log);
+}
+loop(4,0,result=>(result*3)+1,n => n>=0, n => n - 1, console.log)
 
 
       
--- js/tekrar-eden-harfleri-bulmaca.js
+++ js/tekrar-eden-harfleri-bulmaca.js
@@ -1,16 +1,13 @@
 function string(str){
-    let lowstr=str.toLowerCase();
-    let usingSplit =lowstr.split('');
+  let lowstr=str.toLowerCase();
+  let usingSplit =lowstr.split('');
     cnt(usingSplit);
- } 
- 
-    function cnt(arr){
-        let result = {};
-          arr.forEach(function(x) {
-          result[x] = (result[x] || 0) + 1;
-
-            });
-        console.log(result);
+} 
+function cnt(arr){
+  let result = {};
+  arr.forEach(function(x) {
+       result[x] = (result[x] || 0) + 1;});
+  console.log(result);
 }
 
 console.log(string("Hello World"));
