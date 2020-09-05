5c836e996ac7fffdf79d2323647fffb62ec5595c
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-05 19:02:04 +0300

    Add files via upload

--- js/5.2everything.js
+++ js/5.2everything.js
@@ -1,14 +1,13 @@
 function loop(value,test,update,body){
 
   if(test(value) == false){
-     
   }
   else {
    body(value)
    update(value)
   value=update(value);
   loop(value,n => n > 0, n => n - 1, console.log)
-}
+  }
 
 }
 loop(3, n => n > 0, n => n - 1, console.log)
