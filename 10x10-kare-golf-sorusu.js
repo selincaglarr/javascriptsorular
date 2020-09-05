adc083d2bb68cf1a5a388cc38d4b1206095a2a35
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-05 18:04:40 +0300

    Add files via upload

---
+++ 10x10-kare-golf-sorusu.js
@@ -0,0 +1,24 @@
+//kac kez isteniliyorsa o kadar yildiz yazmali 
+let    arrays=[]   
+for(let i=0;i<10;i++){
+      arrays.push(`*`); 
+      arrays=arrays.join("");
+}
+function repeat(n){
+    for(let i=0;i<=n;i++)
+ console.log(arrays);
+}
+repeat(10);
+
+//
+//kac kez isteniliyorsa o kadar yildiz yazmali 
+let    arrays=[]   
+function repeat(n,action){
+    for(let i=0;i<n;i++){
+        arrays.push("*")
+    }
+    for(let k=0; k<n ; k++){
+        action(arrays.join(""))
+    }
+}
+repeat(10,console.log);
