d1b4304871cbb26770bb18dede5a9cc6514b1843
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-11 19:15:14 +0300

    Update not-sistemi-sorusu.js

--- js/not-sistemi-sorusu.js
+++ js/not-sistemi-sorusu.js
@@ -1,6 +1,6 @@
 
 let score=[100,89,79,69,64,59,49,30,0];
-let scoreletter=["AA","BA","BB","CB","CC","DD","FD","FF",];
+let scoreletter=["AA","BA","BB","CB","CC","DD","FD","FF"];
 function compare(scr){
     for(let i=(score.length-1);i>0;i--){
         if(scr<score[i-1] && scr>=score[i]){
