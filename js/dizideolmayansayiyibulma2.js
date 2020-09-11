253f44b891d0f80fbe9d976f8f8cee185ab4b644
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-11 20:11:44 +0300

    Add files via upload

---
+++ js/dizideolmayansayiyibulma2.js
@@ -0,0 +1,17 @@
+let full=[];
+function compare(arr){
+   let max= Math.max(arr); //n eleman sayisi
+    dongu(max) 
+    
+function dongu(n){
+   if(n==0)return;
+   full.push(`n`)
+   dongu(n-1)
+   
+} 
+
+let difference = full.filter(x=>arr.indexOf(x) === -1);
+ console.log(difference);
+}
+ 
+compare([1,2,4,5,6]);
