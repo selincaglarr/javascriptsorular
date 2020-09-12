2c75b1413c737116bf3de75e2e736826a874e784
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-12 23:14:35 +0300

    Update dizide-olmayan-sayiyi-bulma.js

--- js/dizide-olmayan-sayiyi-bulma.js
+++ js/dizide-olmayan-sayiyi-bulma.js
@@ -1,12 +1,10 @@
-let nn=[];
-function compare(arr){
- let n = Math.max(arr); //n eleman sayisi
- 
-  for(let i=1;i<=n;i++){
-     nn.push(i)
-  }
- let difference = nn.filter(x => arr.indexOf(x) === -1);
- console.log(difference);
-  
-};
-console.log(compare([1,2,4,5,6]));
+let newarray=[]
+function array(arr){
+   let max=(Math.max(...arr))
+       for(let i=1;i<=max;i++){
+        newarray.push(i)
+       }
+let difference = newarray.filter(x => arr.indexOf(x) === -1);
+console.log(difference);
+}
+array([1,3,0,7]);
