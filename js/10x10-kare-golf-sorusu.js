a16a78fd6a8628fcc91cdecae451af96713c0bed
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-15 22:01:11 +0300

    Update 10x10-kare-golf-sorusu.js

--- js/10x10-kare-golf-sorusu.js
+++ js/10x10-kare-golf-sorusu.js
@@ -21,3 +21,10 @@ function repeat(n,action){
     }
 }
 repeat(10,console.log);
+//==============================================
+function yildiz(num) {
+	for(let i=0;i<=num;i++)
+	   console.log("*".repeat(num));
+}
+yildiz(10)
+
