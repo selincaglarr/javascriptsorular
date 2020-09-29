9ca9da847d1367a05d35745bfed93c86c6ac27f4
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-09-29 14:22:35 +0300

    Update en-yakin-noktalar-fonksiyonel.js

--- js/en-yakin-noktalar-fonksiyonel.js
+++ js/en-yakin-noktalar-fonksiyonel.js
@@ -1,6 +1,5 @@
 function array(arr){
-  const double = (x,y) => x*x + y*y;	
-  const NewArray = arr.map( subarray => subarray.reduce(double),0);
-    console.log( Math.min(...NewArray))
+  const NewArray = arr.map( subarray => subarray.reduce((x,y) => x*x + y*y),0);
+  console.log(Math.min(...NewArray))
 }
 array([[1,9],[6,1],[3,1],[7,8],[0,9]]);
