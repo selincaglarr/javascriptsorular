ac082bb6b6025f0f2ec101417cc4b69aa02629f2
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-10-01 15:11:57 +0300

    Update ilk-eksik-integer-degeri-bulma.js

--- js/ilk-eksik-integer-degeri-bulma.js
+++ js/ilk-eksik-integer-degeri-bulma.js
@@ -1,5 +1,5 @@
 let fullarray=[]
-function order(arr){
+function Array(arr){
    let currentarray= arr.sort((a,b)=> a-b);
    let max=(Math.max(...arr))
       for(let i=1;i<=max;i++){
@@ -8,4 +8,4 @@ function order(arr){
    let difference = fullarray.find(x => currentarray.indexOf(x) === -1);
    console.log(difference);
 }
-order([1,9,-2,6,3,2,7])
+Array([1,9,-2,6,3,2,7])
