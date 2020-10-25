e8e3f3cdbe1e94a220f0ab3ffeb553cc14bdd3ef
selincaglarr <69199076+selincaglarr@users.noreply.github.com>
2020-10-25 20:28:51 +0300

    Update basiccalculatr.js

--- js/basiccalculatr.js
+++ js/basiccalculatr.js
@@ -6,3 +6,6 @@ function calculator(num1,operator,num2) {
 		'/': num2 ? num1 / num2 : 'Cannot divide by 0!'
 	}[operator]
 }
+
+
+const calculator = (m,o,n) => o+n === "/0" ? 'Cannot divide by 0!' : eval(m+o+n);
